6d0ca662ba9e7a4dfeb1609b44093099b0971318
Jihoonahn <jihoonahn.dev@gmail.com>
2023-12-04 17:19:52 +0900

    Update git

--- Sources/Command/Support/Git.swift
+++ Sources/Command/Support/Git.swift
@@ -6,70 +6,73 @@ public struct Git {
 
     /// Initialize a git repository
     @discardableResult
-    public func `init`() -> Result {
-        return bash.run("git init")
+    public func `init`(at path: String = ".") -> Result {
+        return run(at: path, ["init"])
     }
 
     /// Adds a change in the working directory tothe staging area.
     ///
-    /// - Parameter path: a path
+    /// - Parameter addPath: a path
     @discardableResult
-    public func add(_ path: String = ".") -> Result {
-        return bash.run("git add \(path)")
+    public func add(_ addPath: String = ".", at path: String = ".") -> Result {
+        return run(at: path, ["add", "\(addPath)"])
     }
 
     /// Clone a git repository at a given URL
     @discardableResult
-    public func clone(_ url: URL, to path: String? = nil, allowingPrompt: Bool = true) -> Result {
+    public func clone(_ url: URL, to clonePath: String? = nil, allowingPrompt: Bool = true, at path: String = ".") -> Result {
         var arguments: String = "\(git(allowingPrompt: allowingPrompt)) clone \(url.absoluteString)"
-        path.map { arguments.append(argument: $0) }
+        clonePath.map { arguments.append(argument: $0) }
         arguments.append(" --quiet")
-        let command = Arguments(arguments)
-        return bash.run(command)
+        return run(at: path, [arguments])
     }
 
     /// Create a git commit with given message
     @discardableResult
-    public func commit(_ message: String) -> Result {
+    public func commit(_ message: String, at path: String = ".") -> Result {
         var arguments = "git commit -m"
         arguments.append(argument: message)
         arguments.append(" --quiet")
-        let command = Arguments(arguments)
-        return bash.run(command)
+        return run(at: path, [arguments])
     }
 
     /// Perform a git push
     @discardableResult
-    public func push(remote: String? = nil, branch: String? = nil, allowingPrompt: Bool = true) -> Result {
+    public func push(remote: String? = nil, branch: String? = nil, allowingPrompt: Bool = true, at path: String = ".") -> Result {
         var arguments = "\(git(allowingPrompt: allowingPrompt)) push"
         remote.map { arguments.append(argument: $0) }
         branch.map { arguments.append(argument: $0) }
         arguments.append(" --quiet")
-        let command = Arguments(arguments)
-        return bash.run(command)
+        return run(at: path, [arguments])
     }
-    
+
     /// Perform a git pull
     @discardableResult
-    public func pull(remote: String? = nil, branch: String? = nil, allowingPrompt: Bool = true) -> Result {
+    public func pull(remote: String? = nil, branch: String? = nil, allowingPrompt: Bool = true, at path: String = ".") -> Result {
         var arguments = "\(git(allowingPrompt: allowingPrompt)) pull"
         remote.map { arguments.append(argument: $0) }
         branch.map { arguments.append(argument: $0) }
         arguments.append(" --quiet")
-        let command = Arguments(arguments)
-        return bash.run(command)
+        return run(at: path, [arguments])
     }
-    
+
     /// Checkout a given git branch
     @discardableResult
-    public func checkout(branch: String) -> Result {
-        let arguments = "git checkout"
+    public func checkout(branch: String, at path: String = ".") -> Result {
+        let arguments = "checkout"
             .appending(argument: branch)
             .appending(argument: " --quiet")
-        let command = Arguments(arguments)
-        return bash.run(command)
+        return run(at: path, [arguments])
     }
-    
+
+    /// Running Github Command
+    @discardableResult
+    public func run(at path: String, _ arguments: [String]) -> Result {
+        let command = ["git"] + arguments
+        let arguments = Arguments(command)
+        return bash.run(arguments)
+    }
+
     private func git(allowingPrompt: Bool) -> String {
         return allowingPrompt ? "git" : "env GIT_TERMINAL_PROMPT=0 git"
     }
